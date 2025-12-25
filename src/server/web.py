c104b1be30a0b2c6d3d61b09dd267803380da2ed
akraradets <akraradets@gmail.com>
2025-12-25 16:28:00 +0000

    chore: go back to FastAPI and CloudRun solution Fixes #20
    - remove cli-`main.py`
    - remove `src/main.py`
    - remove unused library
    - add/config FastAPI
    - revert `src/server` to FastAPI

--- src/server/web.py
+++ src/server/web.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, render_template
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-router = Blueprint('web', __name__, template_folder='../templates')
+router = APIRouter()
+# router.mount("/static", StaticFiles(directory="static"), name="static")
 
-@router.route("/", methods=["GET"])
-async def get_index():
-    return render_template("index.html")
 
-@router.route("/example/<int:id>", methods=["GET"])
-async def get_example(id:int):
-    return render_template("example.html", id=id)
+templates = Jinja2Templates(directory="server/templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+@router.get("/example/{id}", response_class=HTMLResponse)
+async def get_example(request: Request, id:int):
+    return templates.TemplateResponse(request=request, name="example.html", context={"id":id})
